edcd0a057148a60a78c4710eee439f9ae058d178
oddlyspookycherry <decentblack0@gmail.com>
2024-05-12 23:09:47 -0400

    Add network test

---
+++ networktest.py
@@ -0,0 +1,39 @@
+import socket
+import time
+from argparse import ArgumentParser
+
+HOST = '0.0.0.0'
+
+def main():
+    arg_parser = ArgumentParser(prog='networktest')
+    arg_parser.add_argument('-peer_ip', type=str, required=True)
+    arg_parser.add_argument('-port', type=int, required=True)
+    arg_parser.add_argument('--owner', action='store_true', required=False)
+    args = arg_parser.parse_args()
+    owner, peer_ip, port = args.owner, args.peer_ip, args.port
+
+    if owner:
+        send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        send_socket.bind((HOST, port))
+        while True:
+            try:
+                send_socket.connect((peer_ip, port))
+                break
+            except:
+                time.sleep(1)
+                continue
+        send_socket.send("hello!".encode())
+    else:
+        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        recv_socket.bind((HOST, port))
+        try:
+            recv_socket.connect((peer_ip, port))
+        except:
+            print("Firewall pinged.")
+        recv_socket.listen(1)
+        owner_socket, owner_address = recv_socket.accept()
+        msg = owner_socket.recv(1024).decode()
+        print(msg)
+
+if __name__ == "__main__":
+    main()
